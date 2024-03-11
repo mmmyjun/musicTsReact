9ca43e9908a15dd9cbee05433f92200d7eb14657
mmmyjun <mmmyjun@outlook.com>
2024-03-11 16:51:38 +0800

    tv: detail嵌入video

--- src/compoents/tv/TvDetail.tsx
+++ src/compoents/tv/TvDetail.tsx
@@ -1,13 +1,13 @@
 import Box from '@mui/material/Box';
 import { useParams } from 'react-router-dom';
-
+import VideoComp from './VideoComp';
 
 function TvDetail() {
     const params = useParams();
     const { id } = params;
     return (
         <Box className="tv-detail-container">
-            详情页开发中id是: {id}
+            <VideoComp />
         </Box>
     );
 }
--- src/compoents/tv/VideoComp.tsx
+++ src/compoents/tv/VideoComp.tsx
@@ -1,14 +1,17 @@
-import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 
 function VideoComp() {
     return (
-        <Box className="video-comp">
+        <Stack className="video-comp" sx={{
+            width: '80%',
+            minWidth: '640px',
+        }}>
             <video controls>
                 <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-        </Box>
+        </Stack>
     );
 }
 
